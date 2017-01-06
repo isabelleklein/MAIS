cc7cd6beacd2efe0d9befd76ca41c212567bef07
soerenhedderich <soeren.hedderich@gmx.de>
2017-01-06 22:56:10 +0100

    Kampangensuche + Vermittlersuche

--- CRM_Entwicklung/pages/Kampagne.php
+++ CRM_Entwicklung/pages/Kampagne.php
@@ -3,113 +3,130 @@
 									<div class="content">
 										<header>
 											<h1>Kampangen</h1>
-											<a href="javascript:AnzeigeUmschalten('new_Kampange');" class="button special">neue Kampange</a>
+											<a href="javascript:AnzeigeUmschalten('Suche');" class="button special">Suche</a>
 										</header>
 									</div>
 									
 								</section>
 								
-								<section id="new_Kampange" style="display:none;">
+						
+								<section id="Suche" style="display:none;">
 									
 									<form action="index.php?page=400" method="post">
 										<table>
 											<tr>
 												<td>Kampangenname</td>
 												<td><input type="text" name="Kampangenname"></td>
-											</tr>
-											<tr>
-												<td>Kampangenart</td>
-												<td><input type="text" name="Kampangenart"></td>
-											</tr>
-											<tr>
-												<td>Kampangenbeginn</td>
-												<td><input type="text" name="Beginn"></td>
-											</tr>
-											<tr>
-												<td>Kampangenende</td>
-												<td><input type="text" name="Ende"></td>
-											</tr>
-											<tr>
-												<td>Kampangenerfasser</td>
-												<td><input type="text" name="Erfasser"></td>
-											</tr>
-											<tr>
-												<td>KampangenID</td>
-												<td><input type="text" name="ID"></td>
-											</tr>
-											<tr>
-												<td>Kampangenbeschreibung</td>
-												<td><input type="text" name="Kampangenbeschreibung"></td>
-											</tr>											
+											</tr>				
 										</table>
-										
-										<input type="submit" value="Speichern">
+										<input type="submit" value="Suche">
 									</form>
 									
-								</section>
-								
-															
+								</section>				
 									<?php
 									if (isset($_POST['Kampangenname'])){
-										$sql = "INSERT INTO Kampange (Kampange_ID, Kampange_Name, Kampange_Beginn, Kampange_Ende, Kampange_Art, Kampange_Erfasser, Kampange_Beschreibung ) VALUES('".$_POST['ID']."','".$_POST['Kampangenname']."','".$_POST['Beginn']."','".$_POST['Ende']."','".$_POST['Kampangenart']."','".$_POST['Erfasser']."','".$_POST['Kampangenbeschreibung']."');";							
+										echo '<section>';
+																			
+											$sql = "SELECT * FROM kampange WHERE Kampange_Name LIKE '%".$_POST['Kampangenname']."%'";
+										
+											$db_erg = mysqli_query($db,$sql);
 											
-										$result = mysqli_query($db, $sql) 
-          											OR die("'".$sql."':".mysqli_error());		
+											if ( ! $db_erg ){
+	  											die('Ungültige Abfrage: ' . mysqli_error($db));
+											}
+											
+											if (is_null($db_erg)){
+											
+												echo "Es wurden keine Kampangen gefunden";
+											
+											}else{
+												
+												?>
+												<table border="1">
+												<thead>
+													<th>Art</th>
+													<th>Beginn</th>
+													<th>Ende</th>
+													<th>Beschreibung</th>
+													<th>Erfasser</th>
+													<th>Name</th>
+													<th></th>
+												</thead>
+												<?php 
+												while ($zeile = mysqli_fetch_array( $db_erg)){	
+		  											echo "<tr>";
+		  											echo "<td>". $zeile['Kampange_Art'] . "</td>";
+		  											echo "<td>". $zeile['Kampange_Beginn'] . "</td>";
+		  											echo "<td>". $zeile['Kampange_Ende'] . "</td>";
+		  											echo "<td>". $zeile['Kampange_Beschreibung'] . "</td>";
+												  	echo "<td>". $zeile['Kampange_Erfasser'] . "</td>";
+												  	echo "<td>". $zeile['Kampange_Name'] . "</td>";
+												  	echo "<td><a href='#' class='button'>Details</a></td>";
+												  	echo "</tr>";
+												}
+												echo "</table>";
+											}
+											mysqli_free_result( $db_erg );
+																			
+										
+										
+										echo '</section>';		
+									}else{
+									
+																
+										echo '<section>';
+																			
+											$sql = 'SELECT * FROM kampange';
+										
+											$db_erg = mysqli_query($db,$sql);
+											
+											if ( ! $db_erg ){
+	  											die('Ungültige Abfrage: ' . mysqli_error($db));
+											}
+											
+											if (is_null($db_erg)){
+											
+												echo "Es wurden keine Kampangen gefunden";
+											
+											}else{
+												
+												?>
+												<table border="1">
+												<thead>
+													<th>Art</th>
+													<th>Beginn</th>
+													<th>Ende</th>
+													<th>Beschreibung</th>
+													<th>Erfasser</th>
+													<th>Name</th>
+													<th></th>
+												</thead>
+												<?php 
+												while ($zeile = mysqli_fetch_array( $db_erg)){	
+		  											echo "<tr>";
+		  											echo "<td>". $zeile['Kampange_Art'] . "</td>";
+		  											echo "<td>". $zeile['Kampange_Beginn'] . "</td>";
+		  											echo "<td>". $zeile['Kampange_Ende'] . "</td>";
+		  											echo "<td>". $zeile['Kampange_Beschreibung'] . "</td>";
+												  	echo "<td>". $zeile['Kampange_Erfasser'] . "</td>";
+												  	echo "<td>". $zeile['Kampange_Name'] . "</td>";
+												  	echo "<td><a href='#' class='button'>Details</a></td>";
+												  	echo "</tr>";
+												}
+												echo "</table>";
+											}
+											mysqli_free_result( $db_erg );
+																			
+										
+										
+										echo '</section>';
 									}
 									?>
 
 
-							<!-- Section Ausgabe-Tabelle Kampangen-->
-								<section>
-									<?php
-										
-										$sql = 'SELECT * FROM kampange';
-									
-										$db_erg = mysqli_query($db,$sql);
-										
-										if ( ! $db_erg ){
-  											die('Ungültige Abfrage: ' . mysqli_error($db));
-										}
-										
-										if (is_null($db_erg)){
-										
-											echo "Es wurden keine Kampangen gefunden";
-										
-										}else{
-											
-											?>
-											<table border="1">
-											<thead>
-												<th>Art</th>
-												<th>Beginn</th>
-												<th>Ende</th>
-												<th>Beschreibung</th>
-												<th>Erfasser</th>
-												<th>Name</th>
-												<th></th>
-											</thead>
-											<?php 
-											while ($zeile = mysqli_fetch_array( $db_erg)){	
-	  											echo "<tr>";
-	  											echo "<td>". $zeile['Kampange_Art'] . "</td>";
-	  											echo "<td>". $zeile['Kampange_Beginn'] . "</td>";
-	  											echo "<td>". $zeile['Kampange_Ende'] . "</td>";
-	  											echo "<td>". $zeile['Kampange_Beschreibung'] . "</td>";
-											  	echo "<td>". $zeile['Kampange_Erfasser'] . "</td>";
-											  	echo "<td>". $zeile['Kampange_Name'] . "</td>";
-											  	echo "<td><a href='#' class='button'>Details</a></td>";
-											  	echo "</tr>";
-											}
-											echo "</table>";
-										}
-										mysqli_free_result( $db_erg );
-																		
-									?>
-									
-								</section>
 								
 								<script language="javascript" type="text/javascript" defer="defer">
-								<!--
+								
 								function AnzeigeUmschalten( i )
 								{
 								   if ( document.getElementById(i).style.display == 'none' ) {
@@ -118,7 +135,7 @@
 								      document.getElementById(i).style.display = 'none';
 								   }
 								}
-								//-->
+
 								</script>
 
 							
--- CRM_Entwicklung/pages/Vermittlersuche.php
+++ CRM_Entwicklung/pages/Vermittlersuche.php
@@ -12,25 +12,65 @@
 													}
 										?>
 									</header>
+								
 
 									<!-- Table -->			
 									<?php
 									if($_GET['msg']=='s'){
 									?>
-									<h3>Suche nach Vermittler ID - <?php echo $Suche; ?></h3>
-									<?php } ?>
-									
-									<?php
-									
-										$Abfrage_1 = "SELECT DISTINCT t1.Vermittler_ID, t2.Vermittler_ZAD, t3.ZAD_Name, t3.ZAD_ORT FROM Vermittler_Konto AS t1 INNER JOIN Vermittler AS t2 ON t1.Vermittler_ID = t2.Vermittler_ID INNER JOIN ZAD AS t3 ON t2.Vermittler_ZAD = t3.ZAD_ZAD"
-									
+									<!-- Suche nach Vermittler Name -->																		
+									<?php 
+											$sql = "SELECT DISTINCT t1.Vermittler_ID, t2.Vermittler_ZAD, t3.ZAD_Name, t3.ZAD_ORT FROM Vermittler_Konto AS t1 INNER JOIN Vermittler AS t2 ON t1.Vermittler_ID = t2.Vermittler_ID INNER JOIN ZAD AS t3 ON t2.Vermittler_ZAD = t3.ZAD_ZAD  WHERE t3.ZAD_Name LIKE '%".$Suche."%'";
+										
+											$db_erg = mysqli_query($db,$sql);
+											
+											if ( ! $db_erg ){
+	  											die('Ungültige Abfrage: ' . mysqli_error($db));
+											}
+											
+											if (!isset($db_erg)){
+												echo "<section>";
+												echo "<h3>Suche nach Vermittler Name</h3>";
+												
+												echo "<table border='1'>";
+												echo "<thead>";
+													echo "<th>Vermittler_ID</th>";
+													echo "<th>Vermittler_ZAD</th>";
+													echo "<th>Vermittler_Name</th>";
+													echo "<th>Vermittler_Ort</th>";
+													echo "<th></th>";
+												echo "</thead>";
+												
+												while ($zeile = mysqli_fetch_array( $db_erg)){	
+		  											echo "<tr>";
+		  											echo "<td>". $zeile['Vermittler_ID'] . "</td>";
+		  											echo "<td>". $zeile['Vermittler_ZAD'] . "</td>";
+		  											echo "<td>". $zeile['ZAD_Name'] . "</td>";
+		  											echo "<td>". $zeile['ZAD_ORT'] . "</td>";
+												  	echo "<td><a href='#' class='button'>auswählen</a></td>";
+												  	echo "</tr>";
+												}
+												echo "</table>";
+												echo "</section>";
+												
+											}
+											mysqli_free_result( $db_erg );
 									?>
-									
-									<h3>Suche nach Vermittler-Name</h3>
 
+									
+									
+									<section>
+									<h3>Suche nach Vermittler-Name</h3>
+									</section>	
+																
+									<section>
 									<h3>Suche nach Vermittler-Konto</h3>
+									</section>
+																	
+									<section>
+									<h3>Suche nach Konto-Name</h3>
+									</section>																
 									
-									<h3>Suche nach Konto-Name</h3>									
-									
-													
-								</section>
+<!-- gehört zum IF msg = s -->		<?php } ?>
+			
+								<!-- </section> -->
